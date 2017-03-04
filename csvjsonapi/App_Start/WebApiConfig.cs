7f39a05ce15fc707b26e5966917ab4e57da4b9fc
Lee Earley <learley@rfx-tech.com>
2017-03-04 14:17:49 -0500

    Implemented static API to deliver json files from filesystem.

--- csvjsonapi/App_Start/WebApiConfig.cs
+++ csvjsonapi/App_Start/WebApiConfig.cs
@@ -5,6 +5,9 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+using Newtonsoft.Json;
 
 namespace csvjsonapi
 {
@@ -25,6 +28,23 @@ namespace csvjsonapi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Add(new BrowserJsonFormatter());
+        }
+    }
+
+    public class BrowserJsonFormatter : JsonMediaTypeFormatter
+    {
+        public BrowserJsonFormatter()
+        {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            this.SerializerSettings.Formatting = Formatting.Indented;
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json");
         }
     }
 }
---
+++ csvjsonapi/Controllers/StaticController.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace csvjsonapi.Controllers
+{
+    [RoutePrefix("api/static")]
+    public class StaticController : ApiController
+    {
+        //// GET: api/Static
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET: api/Static/{dataset}
+        [Route("{jsonfile}")]
+        public object Get(string jsonfile)
+        {
+            string path = @"~\data\" + jsonfile + ".json";
+            string allText = System.IO.File.ReadAllText(path);
+
+            object jsonObject = JsonConvert.DeserializeObject(allText);
+            return jsonObject;
+        }
+
+        //// POST: api/Static
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT: api/Static/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/Static/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
